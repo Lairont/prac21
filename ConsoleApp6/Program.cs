c694319b680e3c4c1a23d85553dccde202ef57ac
Никита <153456515+Lairont@users.noreply.github.com>
2025-02-05 20:08:33 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time clock1 = new Time();
+            clock1.hours = 1;
+            clock1.minutes = 45;
+            clock1.seconds = 14;
+            Console.WriteLine($"{clock1.ToString()}");
+            Console.WriteLine($"Общее количество минут: {clock1.Totalminutes()}\n" +
+                $"Общее количество секунд: {clock1.Totalseconds()}");
+            Console.WriteLine();
+            Time clock2 = new Time(2,10,51);
+            Console.WriteLine($"{clock2.ToString()}");
+            Console.WriteLine($"Общее количество минут: {clock2.Totalminutes()}\n" +
+                $"Общее количество секунд: {clock2.Totalseconds()}");
+            Console.WriteLine();
+            Console.Write("Количество часов: ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Количество минут: ");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.Write("Количество секунд: ");
+            int seconds = int.Parse(Console.ReadLine());
+            Time clock3 = new Time(hours,minutes,seconds);
+            Console.WriteLine($"Общее количество минут: {clock3.Totalminutes()}\n" +
+                $"Общее количество секунд: {clock3.Totalseconds()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp6/Time.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Time
+    {
+        /// <summary>
+        /// поле количества часов
+        /// </summary>
+        public int hours;
+        /// <summary>
+        /// поле количества минут
+        /// </summary>
+        public int minutes;
+        /// <summary>
+        /// поле количества секунд
+        /// </summary>
+        public int seconds;
+        /// <summary>
+        /// конструктор без параметров
+        /// </summary>
+        public Time()
+        { }
+        /// <summary>
+        /// констурктор с одним параметром
+        /// </summary>
+        /// <param name="hours">hours</param>
+        public Time(int hours)
+        {
+            this.hours = hours;
+        }
+        /// <summary>
+        /// конструктор с двумя параметрами
+        /// </summary>
+        /// <param name="hours">hours</param>
+        /// <param name="minutes">minutes</param>
+        public Time(int hours, int minutes) : this(hours)
+        {
+            this.minutes = minutes;
+        }
+        /// <summary>
+        /// конструктор с тремя параметрами
+        /// </summary>
+        /// <param name="hours">hours</param>
+        /// <param name="minutes">minutes</param>
+        /// <param name="seconds">seconds</param>
+        public Time(int hours, int minutes, int seconds) : this(hours, minutes)
+        {
+            this.seconds = seconds;
+        }
+        public override string ToString()
+        {
+            return $"Количество часов: {hours}, Количество минут: {minutes}, Количество секунд: {seconds}";
+        }
+        public int Totalminutes()
+        {
+            return hours * 60 + minutes;
+        }
+        public int Totalseconds()
+        {
+            return (hours * 3600) + (minutes * 60) + seconds;
+        }
+    }
+}
